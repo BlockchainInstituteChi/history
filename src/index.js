6b8f90a3022c7130f14cf9ed91ee27fbab7a8533
alexander-morris <alex@theblockchaininstitute.org>
2019-09-18 11:01:56 -0500

    fixed loading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import HorizontalTimelineContent from './HorizontalTimelineContent';
+import GameInfo from './resources/content';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Directly importing the minified bootstrap css to avoid all the painful
+// steps it will take otherwise to get it to work.
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0, previous: 0 };
+  }
+
+  componentWillMount() {
+    this.data = GameInfo.map((game, index) => {
+      return ({
+        date: game.date,
+        component: (
+          <div className='container' key={index}>
+            <h1>{ `The Elder Scrolls ${index + 1}:`}</h1>
+            <h2>{ game.subtitle }</h2>
+            <hr />
+            <p>{ game.content}</p>
+            <hr />
+          </div>
+        )
+      });
+    });
+  }
+
+  render() {
+    return (
+      <HorizontalTimelineContent
+        content={this.data} />
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('content'));
